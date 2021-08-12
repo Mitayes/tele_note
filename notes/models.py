ec3efd3bb3c7d2659109b8a51b34b569ea81a234
mitayes <mitayes@mail.ru>
2021-08-12 22:16:14 +0500

    initial commit

--- notes/models.py
+++ notes/models.py
@@ -2,11 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
-
-
 class UserNotes(models.Model):
     pass
 
