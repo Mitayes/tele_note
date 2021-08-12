44b73b9993c4d27b91a4e8a1f9d3a83d6a6d974e
mitayes <mitayes@mail.ru>
2021-08-12 23:12:09 +0500

    create and migrate model Topit

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
-class UserNotes(models.Model):
-    pass
+class Topic(models.Model):
+    """Тема, которую просматривает пользователь."""
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    header = models.CharField(max_length=100)
+    tags = models.CharField(max_length=100, blank=True)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_last_saved = models.DateTimeField(auto_now_add=True)
+    # task_type =
 
-
+    def __str__(self):
+        return self.header
