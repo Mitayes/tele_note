37ac95c0f72d95fce89e899ca4c108ce9838a54d
Mitayes <mitayes@mail.ru>
2021-08-16 22:50:11 +0500

    realise display notes for logged user

--- notes/models.py
+++ notes/models.py
@@ -13,4 +13,17 @@ class Topic(models.Model):
     # task_type =
 
     def __str__(self):
-        return self.header
+        # return self.header
+        return '%s %s' % (self.owner, self.header)
+
+    def get_fields(self):
+        fields = {
+            'header': self.header,
+            'tags': self.tags,
+            'text': self.text,
+            'date_added': self.date_added.strftime("%d-%m-%Y %H:%M"),
+            'date_last_saved': self.date_last_saved.strftime("%d-%m-%Y %H:%M"),
+        }
+        return fields
+
+
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from . models import Topic
+import os
+
+api_token = os.getenv('api_token')
 
 
 def index(requests):
-    return render(requests, 'index.html')
+    # Получаем логин залогинённого пользователя
+    # user_name = requests.user.username
+    # Получаем ИД залогинённого пользователя
+    user_id = requests.user.pk
+    # Получаем записи пользователя из БД
+    data = Topic.objects.filter(owner=user_id)
+    context = {
+        'data': data,
+    }
+    return render(requests, 'index.html', context)
